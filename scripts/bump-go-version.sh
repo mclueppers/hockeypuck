7b41333420fd4269ef10ab831ba897dbaecf671d
Andrew Gallagher <andrewg@andrewg.com>
2025-06-22 18:48:14 +0100

    fix up cbc9c5b59

--- scripts/bump-go-version.sh
+++ scripts/bump-go-version.sh
@@ -6,5 +6,5 @@ newversion=$1
 perl -pi.bak -e "s/^(Build-Depends: .*) golang-[0-9.]+/\$1 golang-$newversion/" ./debian/control
 perl -pi.bak -e "s/^FROM golang:[0-9.]+/FROM golang:$newversion/" ./Dockerfile
 perl -pi.bak -e "s/\bGo [0-9.]+/Go $newversion/" ./README.md
-perl -pi.bak -e "s/go-version: '[0-9.]+'/go-version: '$newversion'/" ./README.md
+perl -pi.bak -e "s/go-version: '[0-9.]+'/go-version: '$newversion'/" ./.github/workflows/pr.yml
 perl -pi.bak -e "s,\bgo/[0-9.]+/,go/$newversion/," ./snapcraft.yaml
