7c9d41f3b27a3f50f52a167c4321d1ae7bfed714
Martin Dobrev <martin@dobrev.eu>
2025-06-24 07:50:28 +0100

    Update modules and vendor folder
    Use latest version of dependencies and modules

--- src/hockeypuck/server/settings.go
+++ src/hockeypuck/server/settings.go
@@ -24,7 +24,7 @@ import (
 	"text/template"
 
 	"github.com/BurntSushi/toml"
-	"github.com/Masterminds/sprig"
+	"github.com/Masterminds/sprig/v3"
 	"github.com/pkg/errors"
 
 	"hockeypuck/conflux/recon"
